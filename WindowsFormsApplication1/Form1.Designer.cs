8caab237404be754f52245cbf00aa290055aa54a
Daniel Strom <Daniel@InfiniteMollusk.(none)>
2012-11-06 02:44:46 -0800

    first commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace CuecatDecoder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.CausesValidation = false;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(241, 35);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "das";
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(241, 35);
+            this.ControlBox = false;
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(600, 35);
+            this.MinimumSize = new System.Drawing.Size(100, 35);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+
+
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CuecatDecoder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        // Handle the KeyDown event to determine the type of character entered into the control. 
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Determine whether the keystroke is a enter 
+            if (e.KeyCode == Keys.Enter)
+            {
+                string stringreferencechars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+-";
+                string[] encodedinput1 = this.textBox1.Text.Substring(1,this.textBox1.Text.Length-1).Split('.');
+                string[] encodedinput = encodedinput1[encodedinput1.Length-1].Select(c => c.ToString()).ToArray();
+                string tempvar = "";
+
+                for (int current = 0; current < encodedinput.Length; current++)
+                {
+                    int step5 = 0;
+                    int step15 = 0;
+                    string curchar;
+                    try
+                    {       //yes, you numskulls, I know this is horrible code. deal with it. I'm following the guide at
+                            //http://www.rkgage.net/bobby/cuecat/decode2.htm step by step.
+                        curchar = encodedinput[current]; //get the first character
+                        int step1 = stringreferencechars.IndexOf(curchar); //get the first character's position
+                        int step2 = step1 - 1;
+                        int step3 = step2 << 2;
+                        current++;
+                        curchar = encodedinput[current]; //get the second character
+                        int step4 = stringreferencechars.IndexOf(curchar); //get the second character's position
+                        step5 = step4 - 1;
+                        int step6 = step5 >> 4;
+                        int step7 = step3 | step6;
+                        int step8 = step7 | 3;
+                        int step9 = step8 + 64;
+                        int step10 = (step9 > 128 ? step9 - 128 : step2);
+                        char step11 = (char) step10;
+                        tempvar += step11;
+                    }
+                    catch { }
+                    //step12: end if there's no other character
+                    try
+                    {
+                        int step13 = step5 << 4;
+                        current++;
+                        curchar = encodedinput[current]; //get the third character
+                        int step14 = stringreferencechars.IndexOf(curchar); //get the third character's position
+                        step15 = step14 - 1;
+                        int step16 = step15 << 2;
+                        int step17 = step13 | step16;
+                        int step18 = step17 ^ 3;
+                        int step19 = step18 + 64;
+                        int step20 = (step19 > 128 ? step19 - 128 : step19);
+                        char step21 = (char)step20;
+                        tempvar += step21;
+                    } catch {}
+                    //step 22: end if there's no other character.
+                    try
+                    {
+                        int step23 = step15 << 6;
+                        current++;
+                        curchar = encodedinput[current]; //get the fourth character
+                        int step24 = stringreferencechars.IndexOf(curchar); //get the fourth character's position
+                        int step25 = step24 - 1;
+                        int step26 = step23 | step25;
+                        int step27 = step26 ^ 3;
+                        int step28 = step27 + 64;
+                        char step29 = (char)step28;
+                        tempvar += step29;
+                    }
+                    catch { }
+                }
+                this.textBox1.Text = tempvar;
+            }
+        }
+    }
+}
